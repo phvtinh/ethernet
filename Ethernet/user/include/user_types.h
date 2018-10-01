4b7cb5f7ba3f48aa145f78abcb9c04e017290ac2
lovana <phvtinh2712@gmail.com>
2018-10-01 20:19:42 +0700

    Create ethernet folder

---
+++ Ethernet/user/include/user_types.h
@@ -0,0 +1,20 @@
+#ifndef _USER_TYPES_H_
+#define _USER_TYPES_H_
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long int uinnt64_t;
+
+typedef signed char sint8_t;
+typedef signed short int sint16_t;
+typedef signed int sint32_t;
+typedef signed long int sint64_t;
+typedef float f32_t;
+
+#define UBYTE_MAX    (255)
+#define UBYTE_MIN    (0)
+#define SBYTE_MAX    (127)
+#define SBYTE_MIN    (-128)
+
+#endif //_USER_TYPES_H_
---
+++ Ethernet/user/source/raw_ethernet.cpp
@@ -0,0 +1,11 @@
+#include <raw_ethernet.hpp>
+
+namespace user_ethernet {
+
+    void ethernet::ethernet(const std::string &ifname, uint16_t protocol)
+	{
+	}
+
+}
+
+//end of module ethernet
